592d9676faa14cee1c4256df2c4402ecadd62c5d
Gary <garyjia87@gmail.com>
2024-06-10 08:00:57 -0400

    first try

--- raise/dataset/build_IRAVEN.sh
+++ raise/dataset/build_IRAVEN.sh
@@ -1,4 +1,5 @@
-python dataset/IRAVEN/create_IRAVEN.py \
-  --dataset_root '<Your path to the root of I-RAVEN dataset>' \
+#!/bin/bash
+python /home/bj1/Desktop/generative-abstract-reasoning/raise/dataset/IRAVEN/create_IRAVEN.py \
+  --dataset_root '/home/bj1/Desktop/generative-abstract-reasoning/raise/' \
   --size 64 \
   --output_path './cache'
